2b170faf6f26ffded1b811a83500362cdb18dd34
Adrian Jendo <ajendo@uwaterloo.ca>
2021-11-25 16:50:24 -0800

    more work

--- f.m
+++ f.m
@@ -1,5 +1,10 @@
 % Define hat function
 % Use heaviside(t-T) for heaviside
 function resp = f(t)
-    resp = (heaviside(t + 2) - heaviside(t + 1)) * (t + 2) + (heaviside(t + 1) - heaviside(t - 1)) + (heaviside(t - 1) - heaviside(t - 2)) * (2 - t);
+    resp = (heaviside(t) - heaviside(t - 1)) * (t);
+%     resp = piecewise((t<0) & (t>=-1), -t.^2-t+0.25, (t<1) & (t>=0), t.^2-t+0.25, t>=1 | t<-1, 0);
+%d    resp = (heaviside(t+pi)-heaviside(t+pi-1))*-(t+pi)+(heaviside(t+1)-heaviside(t))*-t+(heaviside(t-pi+1)-heaviside(t-pi))*(t-pi)+(heaviside(t)-heaviside(t-1))*t;
+%e    resp = (heaviside(t+2)-heaviside(t+1))*-(t+2)+(heaviside(t+1)-heaviside(t-1))*t+(heaviside(t-1)-heaviside(t-2))*-(t-2);
+%f    resp = (heaviside(t+pi)-heaviside(t+pi-1))*(-t-pi)+(heaviside(t)-heaviside(t-1))*t;
+%     resp = 3+sqrt(3)*cos(2*t)+sin(2*t)+sin(3*t)-1/2*cos(5*t+pi/3);
 end
--- fourierSeries.m
+++ fourierSeries.m
@@ -4,26 +4,48 @@ clc
 
 % Define domain and variables
 % w0 = 2pi/t0
-w0 = 2 * pi / 6;
-t0 = 6;
-a = -2;
-N = 100;
+w0 = 2*pi;
+t0 = 1;
+a = 0;
+N = 10;
 
 % ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 % Helper variables
-b = a + t0;
+b = 1;
 syms t n
 
 % Compute fourier series
 a0 = 1 / t0 * int(f(t), t, a, b);
 an = 2 / t0 * int(f(t) * cos(n * w0 * t), t, a, b);
 bn = 2 / t0 * int(f(t) * sin(n * w0 * t), t, a, b);
+c0 = a0;
+d0 = c0;
+cn = sqrt(abs(an)+abs(bn));
+theta = atan(-bn/an);
+dn = 1 / t0 * int(f(t)*exp(-1*1i*n*w0*t), t, a, b);
 
 % Plot fourier series
-series = a0;
+trigSeries = a0;
+ci = sym('c',[1 N]); % for error
+xi = sym('x',[1 N]);
 for i = 1:N
-    series = series + subs(an, i) * cos(i * w0 * t) + subs(bn, i) * sin(i * w0 * t);
+    xi(i) = subs(an, i) * cos(i * w0 * t) + subs(bn, i) * sin(i * w0 * t);
+    trigSeries = trigSeries + xi(i);
+    ci(i) = int(f(t)*xi(i), t, a, b)/int(xi(i)^2, t, a, b);
 end
 
-fplot(series, [-2 2])
+expSeries = c0;
+for i = -N:N
+    if i ~= 0
+        expSeries = expSeries + subs(dn, i) * exp(1i * i * w0 * t);
+    end
+end
+
+En = int(f(t)^2, t, a, b) - a0^2;
+for i = 1:N
+    En = En - ci(i)^2*int(xi(i)^2, t, a, b);
+end
+fplot(trigSeries, [a b])
+figure
+fplot(expSeries, [a b])
